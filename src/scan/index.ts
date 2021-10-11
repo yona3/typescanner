567110c73ca3cd70e2447a9ee064e4974e6bf902
yona3 <kou.y.0905@icloud.com>
2021-10-12 00:58:14 +0900

    add: scan

---
+++ src/scan/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @package
+ */
+export const scan = <T>(
+  value: T,
+  condition: (value: unknown) => value is T,
+  callback?: (value: unknown) => void
+): T => {
+  if (condition(value)) return value;
+  if (callback) callback(value);
+  throw new Error(`type assertion is failed. (value: ${value})`);
+};
