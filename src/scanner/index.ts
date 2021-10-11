7a0345582ddf9bba91a290643effd89114526c51
yona3 <kou.y.0905@icloud.com>
2021-10-11 23:15:39 +0900

    refactor: scanner

--- src/scanner/index.ts
+++ src/scanner/index.ts
@@ -1,5 +1,5 @@
-import { isObject } from "./typeGuard";
-import type { Condition } from "./types";
+// scanner
+export { scanner } from "./scanner";
 
 // fields
 export {
@@ -30,26 +30,3 @@ export {
   isSymbol,
   isUndefined,
 } from "./typeGuard";
-
-// scanner
-export const scanner = <T>(fields: {
-  [key in keyof T]: Condition<any> | Condition<any>[];
-}): ((value: unknown) => value is T) => {
-  return (value: unknown): value is T =>
-    // init value (convert all values to unknown)
-    isObject<T>(value) &&
-    // check fields is object
-    typeof fields === "object" &&
-    fields !== null &&
-    // check each value
-    Object.entries<Condition<T> | Condition<T>[]>(fields).every(
-      ([key, condition]) => {
-        if (Array.isArray(condition)) {
-          // if union
-          return condition.some((cond) => cond((value as any)[key]));
-        } else {
-          return condition((value as any)[key]);
-        }
-      }
-    );
-};
