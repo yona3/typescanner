6e5455740940e1c50bd90d81a15d29a9fea435d3
yona3 <kou.y.0905@icloud.com>
2021-10-12 17:54:46 +0900

    fix: scanner fields type

--- src/scanner/index.ts
+++ src/scanner/index.ts
@@ -2,15 +2,16 @@ import { isObject } from "../typeGuard";
 import type { Condition } from "../types";
 
 export const scanner = <T extends Record<string, unknown>>(fields: {
-  [key in keyof Required<T>]: Condition<any> | Condition<any>[];
+  [K in keyof Required<T>]: Condition<T[K]> | Condition<T[K]>[];
 }): ((value: unknown) => value is T) => {
   return (value: unknown): value is T =>
     // check each value
     isObject<T>(value) &&
-    Object.entries<Condition<T> | Condition<T>[]>(fields).every(
-      ([key, condition]) =>
-        Array.isArray(condition)
-          ? condition.some((cond) => cond((value as any)[key]))
-          : condition((value as any)[key])
+    Object.entries<Condition<T[keyof T]> | Condition<T[keyof T]>[]>(
+      fields
+    ).every(([key, condition]) =>
+      Array.isArray(condition)
+        ? condition.some((cond) => cond((value as any)[key]))
+        : condition((value as any)[key])
     );
 };
