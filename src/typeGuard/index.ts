227dcd16c7c0c3fda9215f7b5dbb722a30689896
yona3 <kou.y.0905@icloud.com>
2021-12-05 15:59:54 +0900

    update: primitive type guard function type

--- src/typeGuard/index.ts
+++ src/typeGuard/index.ts
@@ -1,26 +1,30 @@
-import type { WouldBe } from "../types";
+import type { Condition, WouldBe } from "../types";
 
-export const isString = (value: unknown): value is string =>
+export const isString: Condition<string> = (value: unknown): value is string =>
   typeof value === "string";
 
-export const isNumber = (value: unknown): value is number =>
+export const isNumber: Condition<number> = (value: unknown): value is number =>
   typeof value === "number";
 
-export const isBoolean = (value: unknown): value is boolean =>
-  typeof value === "boolean";
+export const isBoolean: Condition<boolean> = (
+  value: unknown
+): value is boolean => typeof value === "boolean";
 
-export const isSymbol = (value: unknown): value is symbol =>
+export const isSymbol: Condition<symbol> = (value: unknown): value is symbol =>
   typeof value === "symbol";
 
-export const isBigint = (value: unknown): value is bigint =>
+export const isBigint: Condition<bigint> = (value: unknown): value is bigint =>
   typeof value === "bigint";
 
-export const isUndefined = (value: unknown): value is undefined =>
-  typeof value === "undefined";
+export const isUndefined: Condition<undefined> = (
+  value: unknown
+): value is undefined => typeof value === "undefined";
 
-export const isNull = (value: unknown): value is null => value === null;
+export const isNull: Condition<null> = (value: unknown): value is null =>
+  value === null;
 
-export const isDate = (value: unknown): value is Date => value instanceof Date;
+export const isDate: Condition<Date> = (value: unknown): value is Date =>
+  value instanceof Date;
 
 export const isArray = <T>(
   array: unknown,
